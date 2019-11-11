d7fa4fc6cf99f1759c7cb04e073e1d1d8429c6ff
Julian Picon <julian.picon@hotmail.com>
2019-11-11 09:15:01 -0500

    Initial unit test

--- src/test/java/co/com/zemoga/api/PortfolioRestControllerTest.java
+++ src/test/java/co/com/zemoga/api/PortfolioRestControllerTest.java
@@ -1,9 +1,14 @@
 package co.com.zemoga.api;
 
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
+//@RunWith(MockitoJUnitRunner.class)
 public class PortfolioRestControllerTest {
+
+    @InjectMocks
+    PortfolioRestController controller;
 
 }
