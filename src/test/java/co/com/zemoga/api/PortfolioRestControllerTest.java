d9f88d1fe7551de19d428c394e19838bc25f6cd0
Julian Picon <julian.picon@hotmail.com>
2019-11-11 10:08:33 -0500

    Added Unit test

--- src/test/java/co/com/zemoga/api/PortfolioRestControllerTest.java
+++ src/test/java/co/com/zemoga/api/PortfolioRestControllerTest.java
@@ -1,14 +1,48 @@
 package co.com.zemoga.api;
 
+import co.com.zemoga.entity.Portfolio;
+import co.com.zemoga.resource.PortfolioResource;
+import co.com.zemoga.service.PortfolioService;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.hateoas.Resources;
 
-//@RunWith(MockitoJUnitRunner.class)
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
 public class PortfolioRestControllerTest {
 
+    @Mock
+    private PortfolioService portfolioService;
     @InjectMocks
-    PortfolioRestController controller;
+    private PortfolioRestController controller;
+
+    private Portfolio portfolio = new Portfolio();
+
+    private static final String USER_NAME = "user1234";
+
+    @Before
+    public void setUp() throws Exception {
+        portfolio.setId(1);
+        portfolio.setTitle("Portfolio Title");
+        when(portfolioService.getByUserName(any())).thenReturn(portfolio);
+        //when(portfolioService.findById(any())).thenReturn(new Portfolio());
+        when(portfolioService.getPortfolios()).thenReturn(new ArrayList<>());
+    }
+
+    @Test
+    public void testRetrievePortfolios() throws Exception {
+        Resources<PortfolioResource> users = controller.getUsers();
+        assertNotNull(users);
+        verify(portfolioService, times(1)).getPortfolios();
+    }
 
 }
